be378f511a8b116d0befbeed96af22a9d2723223
mohamednur <mohamednurhassan8@gmail.com>
2019-09-16 12:08:31 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AppRoutingModule} from './app-routing.modules';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
